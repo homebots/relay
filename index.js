d0e9415713e0f112a2df790b99b57822e83cff34
Darlan Alves <me@darlanalv.es>
2019-08-04 14:06:28 +0200

    feat(*): broadcast new clients

--- index.js
+++ index.js
@@ -4,38 +4,50 @@ const crypto = require('crypto');
 
 const uid = () => {
   const seed = crypto.randomBytes(32);
-  return crypto.createHash('sha256').update(seed).digest('hex');
+  return crypto.createHash('sha256').update(seed).digest('hex').slice(0, 7);
 }
 
-const httpServer = createServer(function(request, response) {
-  const list = [];
-  socket.clients.forEach(client => list.push(client.id));
 
-  response.end(JSON.stringify(list));
+const log = (...args) => process.env.DEBUG ? console.log(...args) : '';
+
+const httpServer = createServer(function(request, response) {
+  if (request.url === '/') {
+    const list = Array.from(socket.clients).map(client => client.id);
+    response.end(JSON.stringify(list));
+  }
+
+  response.writeHead(404, 'Not found');
+  response.end('');
 });
 
 const socket = new WebSocket.Server({
   server: httpServer,
   port: Number(process.env.SOCKET_PORT || 5000),
-  path: '/'
+  path: '/relay'
 });
 
-socket.on('connection', handleConnection);
+const nullOrigin = { id: '0000000' };
+function broadcast(origin, message) {
+  const relayMessage = `${origin.id} ${message}`;
+  const target = message[0] === '@' ? String(message).slice(1, 65) : '';
+  const clients = Array.from(socket.clients)
+    .filter(client => client !== origin && (!target || target === client.id));
 
-function handleConnection(connection) {
-  connection.id = uid();
-  connection.on('close', () => console.log(`${connection.id} disconnected.`));
-  connection.on('message', function(message) {
-    const relayMessage = `${connection.id} ${message}`;
-    const target = message[0] === '@' ? String(message).slice(1, 65) : '';
-    const clients = Array.from(socket.clients)
-      .filter(client => client !== connection && (!target || target === client.id));
-
-    clients.forEach(client => {
-      console.log(`${Date.now()} ${client.id} ${message}`);
-      client.send(relayMessage);
-    });
+  clients.forEach(client => {
+    log(`${Date.now()} ${client.id} ${message}`);
+    client.send(relayMessage);
   });
 }
 
+function handleConnection(connection) {
+  connection.id = uid();
+  connection.on('close', () => broadcast(nullOrigin, `-${connection.id}`));
+  connection.on('message', function(message) {
+    broadcast(connection, message);
+  });
+
+  broadcast(nullOrigin, `+${connection.id}`)
+}
+
+socket.on('connection', handleConnection);
 httpServer.listen(Number(process.env.HTTP_PORT || 80));
