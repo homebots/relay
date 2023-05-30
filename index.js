d7dd05b483da08bf03fdcdfee4ace67e668f0b74
Darlan Alves <me@darlanalv.es>
2023-05-30 23:31:06 +0200

    fix: remove non-websocket API

--- index.js
+++ index.js
@@ -1,61 +1,18 @@
-const http = require('http');
-const https = require('https');
-const WebSocket = require('ws');
-const crypto = require('crypto');
-const Url = require('url');
+const http = require("http");
+const WebSocket = require("ws");
+const crypto = require("crypto");
 
-const httpPort = Number(process.env.PORT || 80);
-const socketPort = Number(process.env.WEBSOCKET_PORT || 3000);
-const useSsl = !!process.env.SSL;
+const port = Number(process.env.PORT);
 const relayMap = new Map();
-const socketServer = (useSsl ? https : http).createServer();
+const socketServer = http.createServer();
 
-const uid = () => {
-  const seed = crypto.randomBytes(32);
-  return crypto.createHash('sha256').update(seed).digest('hex');
-}
-
+const uid = () =>
+  crypto.createHash("sha256").update(crypto.randomBytes(32)).digest("hex");
 const log = (...args) => console.log(`[${new Date().toISOString()}]`, ...args);
 
-const httpServer = http.createServer(function(request, response) {
-  response.setHeader('Access-Control-Allow-Origin', '*');
-  response.setHeader('Access-Control-Request-Method', '*');
-  response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-  response.setHeader('Access-Control-Allow-Headers', 'authorization, content-type');
-
-  if (request.method === 'OPTIONS') {
-    response.writeHead(200);
-    response.end();
-    return;
-  }
-
-  if (request.url === '/reset') {
-    log('Resetting sockets');
-    relayMap.forEach(socket => socket.close());
-    relayMap.clear();
-    response.end('OK');
-    return;
-  }
-
-  if (request.url === '/new') {
-    response.end(uid());
-    return;
-  }
-
-  if (request.url === '/status') {
-    const list = {};
-    relayMap.forEach(socket => list[socket.id] = socket.clients.size);
-    response.end(JSON.stringify(list, null, 2));
-    return;
-  }
-
-  response.writeHead(404, 'Not found');
-  response.end('');
-});
-
-socketServer.on('upgrade', function (request, socket, head) {
-  const pathname = Url.parse(request.url).pathname;
-  const sessionId = pathname.slice(5);
+socketServer.on("upgrade", function (request, socket, head) {
+  const pathname = new URL(request.url, "http://localhost").pathname;
+  const sessionId = pathname.slice(1);
 
   if (!sessionId) {
     socket.destroy();
@@ -66,7 +23,7 @@ socketServer.on('upgrade', function (request, socket, head) {
   let relay = relayMap.get(sessionId);
 
   if (!relay) {
-    relay = new WebSocket.Server({ noServer: true })
+    relay = new WebSocket.Server({ noServer: true });
     relay.id = sessionId;
     relayMap.set(sessionId, relay);
   }
@@ -81,11 +38,14 @@ function broadcast(origin, message) {
   const sessionId = origin.id;
   const socket = relayMap.get(sessionId);
 
-  if (!socket || socket.clients.size < 2) return;
+  if (!socket || socket.clients.size < 2) {
+    return;
+  }
 
-  const hexMessage = typeof message !== 'string' ? message.toString('hex') : message;
+  const hexMessage =
+    typeof message !== "string" ? message.toString("hex") : message;
 
-  socket.clients.forEach(client => {
+  socket.clients.forEach((client) => {
     if (client === origin || client.readyState !== WebSocket.OPEN) return;
 
     if (client.textOnly) {
@@ -100,8 +60,8 @@ function broadcast(origin, message) {
 }
 
 function handleNewClient(socket) {
-  socket.on('message', function(message) {
-    if (message === 'text') {
+  socket.on("message", function (message) {
+    if (message === "text") {
       socket.textOnly = true;
       return;
     }
@@ -120,9 +80,10 @@ function cleanup() {
   });
 }
 
-httpServer.listen(httpPort);
-socketServer.listen(socketPort);
+socketServer.listen(port);
 
 setInterval(cleanup, 5000);
 
-console.log(`[${new Date().toISOString()}] relay running at ${httpPort}, socks at ${socketPort}`);
+console.log(
+  `[${new Date().toISOString()}] relay running at ${httpPort}, socks at ${port}`
+);
